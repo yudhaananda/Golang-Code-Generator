0a41c0dbaf21198fabfb76c332c378dc40fcf3de
Yudha Ananda <yudha.ananda.kresna@bni.co.id>
2022-11-04 16:06:21 +0700

    cors handler

--- main.go
+++ main.go
@@ -32,7 +32,10 @@ func main() {
 		ctx.Data(http.StatusOK, "text/html; charset=utf-8", png)
 	})
 	router.POST("/generate", func(ctx *gin.Context) {
-
+		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 		content, err := ctx.FormFile("file")
 
 		if err != nil {
